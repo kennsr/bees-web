adfcaee06fae5d1eeb05724c7ac1d2876cb36c89
Kennard Sugirotok <kennardfx@gmail.com>
2025-03-11 23:57:09 +0700

    feat: landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,64 +1,66 @@
+import { Menubar } from "@/components/global/navbar";
 import type { Metadata } from "next";
-import { Poppins  } from "next/font/google";
+import { Poppins } from "next/font/google";
 import localFont from "next/font/local";
-import "./globals.css";
 import { Toaster } from "sonner";
-import { Navbar } from "@/components/global/navbar";
-
+import "./globals.css";
+import Image from "next/image";
+import { SiteActions } from "@/components/global/site-actions";
 
 export const metadata: Metadata = {
   title: "Bees Education",
   description: "Learning ain't a chore, it's a lifestyle!",
+  icons: {
+    icon: "/favicon.svg", // Path to your SVG favicon
+  },
 };
 
 const nohemiFont = localFont({
   src: [
     {
-      path: '../fonts/Nohemi-Regular.woff',
+      path: "../fonts/Nohemi-Regular.woff",
       weight: "400",
-      style: 'normal',
+      style: "normal",
     },
     {
-      path: '../fonts/Nohemi-ExtraBold.woff',
+      path: "../fonts/Nohemi-ExtraBold.woff",
       weight: "800",
-      style: 'normal',
+      style: "normal",
     },
     {
-      path: '../fonts/Nohemi-Bold.woff',
+      path: "../fonts/Nohemi-Bold.woff",
       weight: "700",
-      style: 'normal',
+      style: "normal",
     },
     {
-      path: '../fonts/Nohemi-SemiBold.woff',
+      path: "../fonts/Nohemi-SemiBold.woff",
       weight: "600",
-      style: 'normal',
+      style: "normal",
     },
     {
-      path: '../fonts/Nohemi-Medium.woff',
+      path: "../fonts/Nohemi-Medium.woff",
       weight: "500",
-      style: 'normal',
+      style: "normal",
     },
     {
-      path: '../fonts/Nohemi-Light.woff',
+      path: "../fonts/Nohemi-Light.woff",
       weight: "300",
-      style: 'normal',
+      style: "normal",
     },
     {
-      path: '../fonts/Nohemi-ExtraLight.woff',
+      path: "../fonts/Nohemi-ExtraLight.woff",
       weight: "200",
-      style: 'normal',
+      style: "normal",
     },
     {
-      path: '../fonts/Nohemi-Thin.woff',
+      path: "../fonts/Nohemi-Thin.woff",
       weight: "100",
-      style: 'normal',
+      style: "normal",
     },
-    
   ],
   variable: "--font-nohemi",
   display: "swap",
 });
-
 
 const poppins = Poppins({
   variable: "--font-poppins",
@@ -68,24 +70,36 @@ const poppins = Poppins({
   adjustFontFallback: false,
 });
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-  <html lang="en" className={`${nohemiFont.variable} ${poppins.variable} `}>
-      <body
-        className={`h-full w-full antialiased`}
-      >
-        <div className="flex flex-col h-full w-full min-h-screen justify-start ">
-          <div className="h-fit w-full bg-blue-50">
-          <Navbar />
+    <html lang="en" className={`${nohemiFont.variable} ${poppins.variable} `}>
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="64x64" />
+      </head>
+      <body className={`h-full w-full antialiased`}>
+        <div className="flex h-full min-h-screen w-full flex-col justify-start">
+          <div className="relative flex h-[100px] w-full flex-col items-center justify-between overflow-visible px-[20%] pt-[20px]">
+            <div className="flex w-full items-center justify-between">
+              <Image
+                width={100}
+                height={50}
+                className="h-[50px] w-auto"
+                src="/assets/img/bees-logo-full.png"
+                alt="Bees Logo"
+              />
+              <SiteActions />
+            </div>
+
+            {/* SiteActions now overflows downward */}
+            <div className="absolute top-[80%] left-1/2 z-[1] flex h-[50px] w-[80%] -translate-x-1/2 justify-center rounded-xl bg-[var(--foreground)] text-[var(--background)]">
+              <Menubar />
+            </div>
           </div>
-          <div className="flex flex-1 w-full">
-           {children}
-          </div>
+          <div className="flex w-full flex-1">{children}</div>
         </div>
         <Toaster />
       </body>
--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,13 +1,38 @@
 import { FC } from "react";
+
 interface IHomeProps {}
 
-export const Home: FC<IHomeProps> = (props) => {
+export const Home: FC<IHomeProps> = () => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-start">
-      <main id="home">
-        <h1 className="text-3xl font-bold">Bees Education</h1>
-     <p>Learning ain't a chore, it's a lifestyle!</p>
+      <main
+        id="home"
+        className="relative flex h-[50dvh] w-full flex-col items-center justify-center bg-cover bg-center"
+        style={{ backgroundImage: "url('/assets/img/bees-hero.jpg')" }}
+      >
+        <div className="absolute inset-0 bg-black/40"></div>{" "}
+        {/* Optional overlay for readability */}
+        <h1 className="relative z-10 text-3xl font-bold text-white">
+          Bees Education
+        </h1>
+        <p className="relative z-10 text-white">
+          Learning ain't a chore, it's a lifestyle!
+        </p>
       </main>
+      <section
+        className="flex flex-col items-center justify-start p-4"
+        id="about"
+      >
+        <h2 className="text-3xl font-bold">About Us</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
+          distinctio id accusamus accusantium, qui veniam porro consectetur, et
+          molestias pariatur voluptatum hic ipsa mollitia explicabo neque
+          ratione aliquam expedita autem earum dolores dicta? Dolorem obcaecati
+          deserunt quae hic quibusdam a placeat repudiandae alias nostrum minima
+          harum recusandae eaque, aliquid dolores!
+        </p>
+      </section>
     </div>
   );
 };
